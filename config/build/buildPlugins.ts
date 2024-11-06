830f2348155ed8103a2da0bd358f4e8741190fc1
Kamil <kamil.mag1990@gmail.com>
2024-11-06 15:41:15 +0000

    add build project

---
+++ config/build/buildPlugins.ts
@@ -0,0 +1,32 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import webpack from 'webpack'
+import { BuildOptions } from './types/config'
+import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
+
+export function buildPlugins({ paths }: BuildOptions, isDev: boolean): webpack.WebpackPluginInstance[] {
+	const plugins = [
+		new HtmlWebpackPlugin({
+			template: paths.html
+		}),
+		new webpack.ProgressPlugin(),
+		new MiniCssExtractPlugin({
+			filename: 'css/[name].[contenthash:8].css',
+			chunkFilename: 'css/[name].[contenthash:8].css'
+		}),
+		new webpack.DefinePlugin({
+			'process.env.BASE_URL': JSON.stringify(process.env.BASE_URL)
+		})
+	]
+
+	if (isDev) {
+		plugins.push(new webpack.HotModuleReplacementPlugin())
+		plugins.push(
+			new BundleAnalyzerPlugin({
+				openAnalyzer: false
+			})
+		)
+	}
+
+	return plugins
+}
